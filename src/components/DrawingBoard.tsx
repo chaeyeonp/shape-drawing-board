798418b578722097ff569e9bb92ec0c88e6612a7
chaeyeonpk <chaeyeon.park@paysservice.com>
2024-04-02 22:12:37 +0900

    [chore] prettier 및 초기 dependency 설정

--- src/components/DrawingBoard.tsx
+++ src/components/DrawingBoard.tsx
@@ -1,9 +1,9 @@
 const DrawingBoard = () => {
-    return (
-        <div>
-        <h1>Drawing Board</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Drawing Board</h1>
+    </div>
+  );
+};
 
 export default DrawingBoard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import DrawingBoard from "@/components/DrawingBoard";
+import DrawingBoard from '@/components/DrawingBoard';
 
 export default function Home() {
-    return (
-        <>
-            <DrawingBoard/>
-        </>
-    );
+  return (
+    <>
+      <DrawingBoard />
+    </>
+  );
 }
